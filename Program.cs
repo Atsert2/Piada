d9908499a47a9563697a04849043bcd02d6e9e3a
Atsert2 <lia.jp2002@gmai.com>
2025-09-17 20:41:12 -0300

    ...

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Por que o Batman colocou o Bat-móvel no seguro?");Thread.Sleep(4000);
+Console.WriteLine("Porque ele tem medo que Robin!");
